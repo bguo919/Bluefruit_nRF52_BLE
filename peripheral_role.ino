11e302c54968b2fc4d7412b65eea2950da67f85c
bguo919 <56141027+bguo919@users.noreply.github.com>
2023-02-17 01:04:23 -0500

    Add files via upload

---
+++ peripheral_role.ino
@@ -0,0 +1,328 @@
+/*********************************************************************
+ This is an example for our nRF52 based Bluefruit LE modules
+ Pick one up today in the adafruit shop!
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+#include <bluefruit.h>
+#include <Adafruit_LittleFS.h>
+#include <InternalFileSystem.h>
+// SPDX-FileCopyrightText: 2020 Carter Nelson for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+//
+#include <Adafruit_APDS9960.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_LIS3MDL.h>
+#include <Adafruit_LSM6DS33.h>
+#include <Adafruit_SHT31.h>
+#include <Adafruit_Sensor.h>
+#include <PDM.h>
+
+Adafruit_APDS9960 apds9960; // proximity, light, color, gesture
+Adafruit_BMP280 bmp280;     // temperautre, barometric pressure
+Adafruit_LIS3MDL lis3mdl;   // magnetometer
+Adafruit_LSM6DS33 lsm6ds33; // accelerometer, gyroscope
+Adafruit_SHT31 sht30;       // humidity
+
+uint8_t proximity;
+uint16_t r, g, b, c;
+float temperature, pressure, altitude;
+float magnetic_x, magnetic_y, magnetic_z;
+float accel_x, accel_y, accel_z;
+float gyro_x, gyro_y, gyro_z;
+float humidity;
+int32_t mic;
+
+extern PDMClass PDM;
+short sampleBuffer[256];  // buffer to read samples into, each sample is 16-bits
+volatile int samplesRead; // number of samples read
+
+// BLE Service
+BLEDfu  bledfu;  // OTA DFU service
+BLEDis  bledis;  // device information
+BLEUart bleuart; // uart over ble
+BLEBas  blebas;  // battery
+
+void setup()
+{
+
+  Serial.println("Feather Sense Sensor Demo");
+
+  // initialize the sensors
+  apds9960.begin();
+  apds9960.enableProximity(true);
+  apds9960.enableColor(true);
+  bmp280.begin();
+  lis3mdl.begin_I2C();
+  lsm6ds33.begin_I2C();
+  sht30.begin();
+  PDM.onReceive(onPDMdata);
+  PDM.begin(1, 16000);
+
+  Serial.begin(115200);
+
+#if CFG_DEBUG
+  // Blocking wait for connection when debug mode is enabled via IDE
+  while ( !Serial ) yield();
+#endif
+  
+  Serial.println("Bluefruit52 BLEUART Example");
+  Serial.println("---------------------------\n");
+
+  // Setup the BLE LED to be enabled on CONNECT
+  // Note: This is actually the default behavior, but provided
+  // here in case you want to control this LED manually via PIN 19
+  Bluefruit.autoConnLed(true);
+
+  // Config the peripheral connection with maximum bandwidth 
+  // more SRAM required by SoftDevice
+  // Note: All config***() function must be called before begin()
+  Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
+
+  Bluefruit.begin();
+  Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values
+  //Bluefruit.setName(getMcuUniqueID()); // useful testing with multiple central connections
+  Bluefruit.Periph.setConnectCallback(connect_callback);
+  Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+
+  // To be consistent OTA DFU should be added first if it exists
+  bledfu.begin();
+
+  // Configure and Start Device Information Service
+  bledis.setManufacturer("Adafruit Industries");
+  bledis.setModel("Bluefruit Feather52");
+  bledis.begin();
+
+  // Configure and Start BLE Uart Service
+  bleuart.begin();
+
+  // Start BLE Battery Service
+  blebas.begin();
+  blebas.write(100);
+
+  // Set up and start advertising
+  startAdv();
+
+  Serial.println("Please use Adafruit's Bluefruit LE app to connect in UART mode");
+  Serial.println("Once connected, enter character(s) that you wish to send");
+}
+
+void startAdv(void)
+{
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+
+  // Include bleuart 128-bit uuid
+  Bluefruit.Advertising.addService(bleuart);
+
+  // Secondary Scan Response packet (optional)
+  // Since there is no room for 'Name' in Advertising packet
+  Bluefruit.ScanResponse.addName();
+  
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   * 
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
+}
+
+void loop() {
+  proximity = apds9960.readProximity();
+  while (!apds9960.colorDataReady()) {
+    delay(5);
+  }
+  apds9960.getColorData(&r, &g, &b, &c);
+
+  temperature = bmp280.readTemperature();
+  pressure = bmp280.readPressure();
+  altitude = bmp280.readAltitude(1013.25);
+
+  lis3mdl.read();
+  magnetic_x = lis3mdl.x;
+  magnetic_y = lis3mdl.y;
+  magnetic_z = lis3mdl.z;
+
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+  lsm6ds33.getEvent(&accel, &gyro, &temp);
+  accel_x = accel.acceleration.x;
+  accel_y = accel.acceleration.y;
+  accel_z = accel.acceleration.z;
+  gyro_x = gyro.gyro.x;
+  gyro_y = gyro.gyro.y;
+  gyro_z = gyro.gyro.z;
+
+  humidity = sht30.readHumidity();
+
+  samplesRead = 0;
+  mic = getPDMwave(4000);
+
+  proximity = apds9960.readProximity();
+  char buf[32] = {0};
+  itoa(proximity, buf, 10);
+  int count = numDigits(proximity);
+  buf[count] = '\n';
+  bleuart.write(buf, count);
+  
+  Serial.write(buf, count);
+  delay(1000);
+
+  Serial.println("\nFeather Sense Sensor Demo");
+  Serial.println("---------------------------------------------");
+  Serial.print("Proximity: ");
+  Serial.println(apds9960.readProximity());
+  Serial.print("Red: ");
+  Serial.print(r);
+  Serial.print(" Green: ");
+  Serial.print(g);
+  Serial.print(" Blue :");
+  Serial.print(b);
+  Serial.print(" Clear: ");
+  Serial.println(c);
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" C");
+  Serial.print("Barometric pressure: ");
+  Serial.println(pressure);
+  Serial.print("Altitude: ");
+  Serial.print(altitude);
+  Serial.println(" m");
+  Serial.print("Magnetic: ");
+  Serial.print(magnetic_x);
+  Serial.print(" ");
+  Serial.print(magnetic_y);
+  Serial.print(" ");
+  Serial.print(magnetic_z);
+  Serial.println(" uTesla");
+  Serial.print("Acceleration: ");
+  Serial.print(accel_x);
+  Serial.print(" ");
+  Serial.print(accel_y);
+  Serial.print(" ");
+  Serial.print(accel_z);
+  Serial.println(" m/s^2");
+  Serial.print("Gyro: ");
+  Serial.print(gyro_x);
+  Serial.print(" ");
+  Serial.print(gyro_y);
+  Serial.print(" ");
+  Serial.print(gyro_z);
+  Serial.println(" dps");
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+  Serial.print("Mic: ");
+  Serial.println(mic);
+  delay(300);
+
+  // Forward data from HW Serial to BLEUART
+  while (Serial.available())
+  {
+    // Delay to wait for enough input, since we have a limited transmission buffer
+    delay(2);
+
+    uint8_t buf[64];
+    int count = Serial.readBytes(buf, sizeof(buf));
+    //bleuart.write( buf, count );
+  }
+
+  // Forward from BLEUART to HW Serial
+  while ( bleuart.available() )
+  {
+    uint8_t ch;
+    ch = (uint8_t) bleuart.read();
+    Serial.write(ch);
+  }
+}
+
+
+// callback invoked when central connects
+void connect_callback(uint16_t conn_handle)
+{
+  // Get the reference to current connection
+  BLEConnection* connection = Bluefruit.Connection(conn_handle);
+
+  char central_name[32] = { 0 };
+  connection->getPeerName(central_name, sizeof(central_name));
+
+  Serial.print("Connected to ");
+  Serial.println(central_name);
+}
+
+
+/**
+ * Callback invoked when a connection is dropped
+ * @param conn_handle connection where this event happens
+ * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
+ */
+void disconnect_callback(uint16_t conn_handle, uint8_t reason)
+{
+  (void) conn_handle;
+  (void) reason;
+
+  Serial.println();
+  Serial.print("Disconnected, reason = 0x"); Serial.println(reason, HEX);
+}
+
+
+/*****************************************************************/
+int32_t getPDMwave(int32_t samples) {
+  short minwave = 30000;
+  short maxwave = -30000;
+
+  while (samples > 0) {
+    if (!samplesRead) {
+      yield();
+      continue;
+    }
+    for (int i = 0; i < samplesRead; i++) {
+      minwave = min(sampleBuffer[i], minwave);
+      maxwave = max(sampleBuffer[i], maxwave);
+      samples--;
+    }
+    // clear the read count
+    samplesRead = 0;
+  }
+  return maxwave - minwave;
+}
+
+
+void onPDMdata() {
+  // query the number of bytes available
+  int bytesAvailable = PDM.available();
+
+  // read into the sample buffer
+  PDM.read(sampleBuffer, bytesAvailable);
+
+  // 16-bit, 2 bytes per sample
+  samplesRead = bytesAvailable / 2;
+}
+
+
+int numDigits(uint8_t input){
+  int ans = 0;
+  if (input == 0) {
+    ans = 1;
+  }
+  while (input !=0) {
+    ans++;
+    input = input / 10;
+  }
+  return ans;
+}
